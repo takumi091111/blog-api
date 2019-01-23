73c3c4c9e9b6bb355ae9454554dfa8d2c9f5affb
Asamac <takumi091111@gmail.com>
2019-01-23 23:11:29 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const fetch = require('node-fetch')
+const app = express()
+
+const SPACE_ID = process.env.SPACE_ID
+const TOKEN = process.env.TOKEN
+
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://asamac.netlify.com')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-Csrftoken, Content-Type, Accept, Authorization')
+  res.header('Access-Control-Allow-Methods', 'GET')
+  res.header('Access-Control-Max-Age', 3600)
+  next()
+})
+
+app.get('/entries/list', async (req, res) => {
+  const entries = await fetchEntries({
+    limit: req.query.limit,
+    skip: req.query.skip
+  })
+  res.send(entries)
+})
+
+app.get('/entries/:id', async (req, res) => {
+  const entryId = req.params.id
+  const entry = await fetchEntry(entryId)
+  res.send(entry)
+})
+
+const fetchEntries = async ({ limit = 10, skip = 0 }) => {
+  const BASE_URL = 'https://cdn.contentful.com'
+  const endPoint = `/spaces/${SPACE_ID}/environments/master/entries`
+  const url = BASE_URL + endPoint
+
+  const params = {
+    order: '-sys.createdAt',
+    limit: limit,
+    skip: skip
+  }
+
+  const querys = Object.keys(params).map(key => `${key}=${params[key]}`).join('&')
+
+  const response = await fetch(`${url}?${querys}`, {
+    method: 'GET',
+    headers: { 'Authorization': `Bearer ${TOKEN}` }
+  })
+  return response.json()
+}
+
+const fetchEntry = async (entryId) => {
+  const BASE_URL = 'https://cdn.contentful.com'
+  const endPoint = `/spaces/${SPACE_ID}/environments/master/entries/${entryId}`
+  const url = BASE_URL + endPoint
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: { 'Authorization': `Bearer ${TOKEN}` }
+  })
+  return response.json()
+}
+
+app.listen(process.env.PORT || 3000)
